ca95431d75f319f42b320b3dc9fa6fe258675649
Chloé <chloe.du.16@live.fr>
2025-07-31 23:34:06 +0200

    Add host in mai

--- main.py
+++ main.py
@@ -133,4 +133,5 @@ def get_depots():
 
 # -------------------------- Lancement ------------------------ #
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))  # Railway fournit le port
+    app.run(host="0.0.0.0", port=port)
